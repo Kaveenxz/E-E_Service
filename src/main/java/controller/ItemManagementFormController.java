582e076b271bc0450a87c3eb03455949fec59d8d
Kaveenxz <kaveenhansithx@gmail.com>
2024-01-09 16:50:58 +0530

    Item Management UI

---
+++ src/main/java/controller/ItemManagementFormController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.layout.GridPane;
+
+public class ItemManagementFormController {
+
+    @FXML
+    private GridPane pane;
+
+    @FXML
+    private TextField nametxt;
+
+    @FXML
+    private JFXComboBox<?> category;
+
+    @FXML
+    private JFXTreeTableView<?> tblItem;
+
+    @FXML
+    private TreeTableColumn<?, ?> colId;
+
+    @FXML
+    private TreeTableColumn<?, ?> colName;
+
+    @FXML
+    private TreeTableColumn<?, ?> colCategory;
+
+    @FXML
+    private TreeTableColumn<?, ?> colOption;
+
+    @FXML
+    void backButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateBtnOnAction(ActionEvent event) {
+
+    }
+
+}
