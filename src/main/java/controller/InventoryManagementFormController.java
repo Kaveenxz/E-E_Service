64329af99f9d260a4e66eff666734b0572733cca
Kaveenxz <kaveenhansithx@gmail.com>
2024-01-09 22:02:29 +0530

    Inventory management UI

--- src/main/java/controller/InventoryManagementFormController.java
+++ src/main/java/controller/InventoryManagementFormController.java
@@ -1,4 +1,44 @@
 package controller;
 
-public class InventoryManagementFormController {
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.layout.GridPane;
+
+public class InventoryManagementFormController {@FXML
+private GridPane pane;
+
+    @FXML
+    private JFXComboBox<?> category;
+
+    @FXML
+    private JFXTreeTableView<?> tblItem;
+
+    @FXML
+    private TreeTableColumn<?, ?> colName;
+
+    @FXML
+    private TreeTableColumn<?, ?> colCategory;
+
+    @FXML
+    private TreeTableColumn<?, ?> colQty;
+
+    @FXML
+    private TreeTableColumn<?, ?> colStatus;
+
+    @FXML
+    private TreeTableColumn<?, ?> colOption;
+
+    @FXML
+    void backButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveBtnOnAction(ActionEvent event) {
+
+    }
+
 }
