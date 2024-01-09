add516fa8d07c7676e56e01082b54a73be9def78
Kaveenxz <kaveenhansithx@gmail.com>
2024-01-09 21:54:47 +0530

    Reports UI

---
+++ src/main/java/controller/ReportsFormController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class ReportsFormController {
+    @FXML
+    private GridPane pane;
+
+    @FXML
+    void backButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void customerReportBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void orderReportBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salesReportBtnOnAction(ActionEvent event) {
+
+    }
+
+}
+
---
+++ src/main/java/controller/SalesReportsFormController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.layout.GridPane;
+
+public class SalesReportsFormController {
+    @FXML
+    private GridPane pane;
+
+    @FXML
+    private BarChart<?, ?> lineChart;
+
+    @FXML
+    private PieChart pieChart;
+
+    @FXML
+    void annualReportBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dailyReportBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void monthlyReportBtnOnAction(ActionEvent event) {
+
+    }
+
+}
